5f496b34b0e6d4de2bc811339b619f1737f5efef
Sudipto Ghosh <sudiptoghosh@Sudiptos-MacBook-Air.local>
2023-10-31 21:32:04 +0530

    Minor bug fix in App Controller.

--- src/main/java/com/example/controller/AppController.java
+++ src/main/java/com/example/controller/AppController.java
@@ -1,9 +1,21 @@
 package com.example.controller;
 
 import com.example.handler.InsuranceCartHandler;
-import com.example.model.*;
+import com.example.model.AddPolicyToCartRequest;
+import com.example.model.AddPolicyToCartResponse;
+import com.example.model.DeletePolicyFromCartRequest;
+import com.example.model.DeletePolicyFromCartResponse;
+import com.example.model.CreateOrderFromCartResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/insurance/cart")
@@ -24,6 +36,6 @@ public class AppController {
 
     @GetMapping("/createOrder/{userId}")
     public CreateOrderFromCartResponse createOrder(@PathVariable final int userId) {
-        insuranceCartHandler.createOrderFromCart(userId);
+        return insuranceCartHandler.createOrderFromCart(userId);
     }
 }
